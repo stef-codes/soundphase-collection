502d5c36fccd62f9f92d6b5d97ec4961367ba9a7
Stefanie <stefcodes@gmail.com>
2023-01-03 01:11:48 -0700

    change to testnet

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 import "./styles/globals.css";
 
 // This is the chainId your dApp will work on.
-const activeChainId = ChainId.Mainnet;
+const activeChainId = ChainId.Mumbai;
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
